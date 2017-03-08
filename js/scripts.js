b929ef01cf5605d46592ccb013bbba9d75bd291c
Christian Martinez <cgmartinez11142@gmail.com>
2017-03-08 13:21:44 -0800

    return the js back to functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,4 @@
 //business logic
-function cutUp(string, splitType) {
-  var array = [];
-  var newString = string;
-  debugger;
-  if (splitType === "sentence") {
-    for (var i = 0; i < string.length; i++) {
-      var index = string.charAt(i)
-      console.log(index);
-      if (string.charAt(i) === "." || string.charAt(i) === "!" || string.charAt(i) === "?" || string.charAt(i) === "..." || string.charAt(i) === ":") {
-          var newString = newString.replace(index, index + "|");
-      }
-    }
-    array = newString.split("|");
-  } else if (splitType === "word") {
-    array = string.split(" ");
-  }
-}
-
 function shuffle(array) {
   var currentIndex = array.length;
   var temporaryValue;
@@ -41,38 +23,30 @@ $(document).ready(function() {
     $("#replaceWell1").show();
     $("#getStartedExplanation").show();
     $("#userInput").show();
-debugger;
+
     var title1 = ("To Make A Dadist Poem");
     var titleArray = title1.split(" ");
     var jumbledTitle = shuffle(titleArray);
     $(".jumbledTitle").text(jumbledTitle);
-    console.log(cutUp("This is a test. Or is it? It is!", "sentence"));
-    //debugger;
 
+    //this jumbles intro paragraph
     var instructions = $("#paragraph").html();
     var instructArray = instructions.split(" ");
-    var newInstructArray = [];
-    //debugger;
     console.log(instructions);
     console.log(instructArray);
-
-    instructArray.forEach(function(i) {
-      //BUG!!!
-        if (i != "<br>↵") {
-          newInstructArray.push(i)
-        }
-      });
-    //console.log(newInstructArray);
-
-    var jumbledInstruct = shuffle(newInstructArray);
+    var jumbledInstruct = shuffle(instructArray);
     $("#jumbledInstruct").text(jumbledInstruct);
   });
 
-  $("#jumble").click(function(event) {
-    //debugger;
+  $("#jumbleButton").click(function() {
     var input = $("#userText").val();
     var wordArray = input.split(" ");
     var wordResult = shuffle(wordArray);
-    console.log(wordResult);
+    $("#jumbleResult").text(wordResult);
+    $("#jumbledWell").show();
+  });
+
+  $("#clearButton").click(function() {
+    document.getElementById("userText").value = "";
   });
 });
